81b34194886159c69c95d1534271f54454f1633d
Joshua Hartwell <JHartwell@gmail.com>
2020-04-18 12:12:44 -0700

    Update virtual group sittings page

--- page-virtual-group-sitting.php
+++ page-virtual-group-sitting.php
@@ -22,11 +22,13 @@
       document.getElementById( "audio-three").style.display = "none";
       document.getElementById( "audio-four").style.display = "none";
       document.getElementById( "audio-five").style.display = "none";
+      document.getElementById( "audio-six").style.display = "none";
       document.getElementById( "audio-one").pause();
       document.getElementById( "audio-two").pause();
       document.getElementById( "audio-three").pause();
       document.getElementById( "audio-four").pause();
       document.getElementById( "audio-five").pause();
+      document.getElementById( "audio-six").pause();
 
       switch (playerIndex) {
          case 0:
@@ -44,19 +46,50 @@
          case 4:
             document.getElementById( "audio-five").style.display = "block";
             break;
+         case 5:
+            document.getElementById( "audio-six").style.display = "block";
+            break;
       }
    }
+
+   function changeBackgroundTo(backgroundIndex) {
+      imageFileName = "1.png";
+      switch (backgroundIndex) {
+         case 0:
+            imageFileName = "1.png";
+            break;
+         case 1:
+            imageFileName = "2.jpg";
+            break;
+         case 2:
+            imageFileName = "3.jpg";
+            break;
+         case 3:
+            imageFileName = "4.jpg";
+            break;
+         case 4:
+            imageFileName = "5.jpg";
+            break;
+         case 5:
+            imageFileName = "6.jpg";
+            break;
+      }
+      document.getElementById( "vgs-body").style.backgroundImage = "url('/filebase/virtual-group-sittings/backgrounds/" + imageFileName + "')";
+   }
+
    window.onload = function() {
       document.getElementById("session-chooser").onchange = function() {
          changeAudioPlayerTo( this.selectedIndex );
+      }
+      document.getElementById("background-chooser").onchange = function() {
+         changeBackgroundTo( this.selectedIndex );
       }
    }
 </script>
 <?php wp_head(); ?>
 </head>
-<body>
+<body id="vgs-body">
 <div id="intro-text">
-<a href="http://dhara.dhamma.org/os/">&lsaquo;- return to dhara.dhamma.org</a>
    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
       <?php the_content(__('(more...)')); ?>
    <?php endwhile; endif; ?>
@@ -78,15 +111,26 @@
    <source src="/filebase/virtual-group-sittings/audio-5.mp3" type="audio/mpeg">
 </audio>
 <div id="chooser">
-   <label for="session-chooser">Change Audio Session:</label>
+   <label for="session-chooser">Audio Session:</label>
    <select id="session-chooser">
       <option>1 - Dhamma Khetta Short</option>
       <option>2 - Dhamma Giri Minimal Instructions</option>
-      <option>3 - Dhamma Salila Long Instructions</option>
-      <option>4 - Dhamma Salila Short Instructions</option>
-      <option>5 - Dhamma Setu Long Instructions</option>
+      <option>3 - Dhamma Salila Short Instructions</option>
+      <option>4 - Dhamma Setu Long Instructions</option>
+      <option>5 - Dhamma Sikhara Short Instructions</option>
    </select>
    <button id="toggle-text-button" type="button" onclick="toggleTextHide()">Hide Text</button> 
+   <br />
+   <br />
+   <label for="background-chooser">Background:</label>
+   <select id="background-chooser">
+      <option>1 - Dharā Arial</option>
+      <option>2 - Dharā Tree</option>
+      <option>3 - Dharā Gong</option>
+      <option>4 - Patapa Lotus</option>
+      <option>5 - Patapa Sign</option>
+      <option>6 - Patapa Dhamma Hall</option>
+   </select>
 </div>
 </div>
 </body>
